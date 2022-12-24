af8b73f5bd5a10cc2051ce07a1adba1ff90bf362
FatimeIbrahimova <fatime.ibrahimova44@gmail.com>
2022-12-24 19:11:23 +0400

    changes

--- src/App.js
+++ src/App.js
@@ -2,30 +2,39 @@
 // import './App.css';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-import { useRef,useState } from 'react';
+import { useRef, useState } from 'react';
 
 
 function App() {
   const timerRef = useRef(0);
-  const [start,setStart]=useState(false);
-  const [stop,setStop]=useState(false);
-  const [reset,setReset]=useState(false);
-  const [count,setCount]=useState(0);
-  const startHandle=()=>{
-    if(timerRef.current) {return;}
-    timerRef.current= setInterval(() => {
-      setCount(count=>count+1);
+  const [start, setStart] = useState(false);
+  const [stop, setStop] = useState(false);
+  const [reset, setReset] = useState(false);
+  const [count, setCount] = useState(0);
+  const startHandle = () => {
+    if (timerRef.current) { return; }
+    timerRef.current = setInterval(() => {
+      setCount(count => count + 1);
     }, 1000);
+    setStart(true);
+    setStop(false);
+    setReset(false);
   }
-  const stopHandle=()=>{
+  const stopHandle = () => {
     clearInterval(timerRef.current);
     console.log("clic");
-    timerRef.current=0;
+    timerRef.current = 0;
+    setStart(false);
+    setStop(true);
+    setReset(false);
   }
-const resetHandle=()=>{
-  clearInterval(timerRef.current);
-  setCount(0);
-}
+  const resetHandle = () => {
+    clearInterval(timerRef.current);
+    setCount(0);
+    setStart(false);
+    setStop(false);
+    setReset(true);
+  }
 
   return (
     <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100vh" }}>
@@ -35,10 +44,10 @@ const resetHandle=()=>{
         <Button onClick={() => startHandle()} variant={start ? "outlined" : "contained"} color="success">
           Start
         </Button>
-        <Button onClick={()=>stopHandle()} variant={stop ? "outlined" : "contained"} color="error">
+        <Button onClick={() => stopHandle()} variant={stop ? "outlined" : "contained"} color="error">
           Stop
         </Button>
-        <Button onClick={()=>resetHandle()} variant={reset ? "outlined" : "contained"} color="warning">Reset</Button>
+        <Button onClick={() => resetHandle()} variant={reset ? "outlined" : "contained"} color="warning">Reset</Button>
       </Stack>
     </div>
   );
